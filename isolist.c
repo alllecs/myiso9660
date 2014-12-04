2412d20644eb8adf69f0755732d29a2420580862
Smirnov Alexander <alllecs@yandex.ru>
2014-12-05 00:19:18 +0400

    Убрано лишнее в тексте программы

--- isolist.c
+++ isolist.c
@@ -26,47 +26,20 @@
  */
 
 /* Set up a CD-DA image to test on which is in the libcdio distribution. */
-#define ISO9660_IMAGE_PATH "../test/"
-#define ISO9660_IMAGE ISO9660_IMAGE_PATH "copying.iso"
 
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#define __CDIO_CONFIG_H__ 1
-#endif
-
-#ifdef HAVE_STDIO_H
 #include <stdio.h>
-#endif
-#ifdef HAVE_STDLIB_H
 #include <stdlib.h>
-#endif
-#ifdef HAVE_STRING_H
 #include <string.h>
-#endif
-#ifdef HAVE_UNISTD_H
 #include <unistd.h>
-#endif
-#ifdef HAVE_SYS_TYPES_H
 #include <sys/types.h>
-#endif
 
 #include <cdio/cdio.h>
 #include <cdio/iso9660.h>
-#define print_vd_info(title, fn)
-	if (fn(p_iso, &psz_str)) {
-	printf(title ": %s\n", psz_str);
-	}					  \
-	free(psz_str);			  \
-	psz_str = NULL;
-
 
 int main(int argc, const char *argv[])
 {
-	CdioList_t *p_entlist;
-	CdioListNode_t *p_entnode;
 	char const *psz_fname;
 	iso9660_t *p_iso;
-	const char *psz_path="/";
 
 	if (argc > 1)
 		psz_fname = argv[1];
@@ -76,38 +49,9 @@ int main(int argc, const char *argv[])
 	p_iso = iso9660_open (psz_fname);
 
 	if (NULL == p_iso) {
-		printf(stderr, "Sorry, couldn't open %s as an ISO-9660 image\n", psz_fname);
+		printf("Sorry, couldn't open %s as an ISO-9660 image\n", psz_fname);
 		return 1;
 	}
-
-  /* Show basic CD info from the Primary Volume Descriptor. */
-	{
-		char *psz_str = NULL;
-		print_vd_info("Application", iso9660_ifs_get_application_id);
-		print_vd_info("Preparer   ", iso9660_ifs_get_preparer_id);
-		print_vd_info("Publisher  ", iso9660_ifs_get_publisher_id);
-		print_vd_info("System     ", iso9660_ifs_get_system_id);
-		print_vd_info("Volume     ", iso9660_ifs_get_volume_id);
-		print_vd_info("Volume Set ", iso9660_ifs_get_volumeset_id);
-	}
-
-	p_entlist = iso9660_ifs_readdir (p_iso, psz_path);
-
-  /* Iterate over the list of nodes that iso9660_ifs_readdir gives  */
-
-	if (p_entlist) {
-		_CDIO_LIST_FOREACH (p_entnode, p_entlist)
-		{
-			char filename[4096];
-			iso9660_stat_t *p_statbuf =
-			(iso9660_stat_t *) _cdio_list_node_data (p_entnode);
-			iso9660_name_translate(p_statbuf->filename, filename);
-			printf ("%s [LSN %6d] %8u %s%s\n", _STAT_DIR == p_statbuf->type ? "d" : "-", p_statbuf->lsn, p_statbuf->size, psz_path, filename);
-		}
-
-		_cdio_list_free (p_entlist, true);
-	}
-
 
 	iso9660_close(p_iso);
 	return 0;

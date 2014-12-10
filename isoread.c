b8d6ae48aca2cd0fc7d1ad0b2441652a53a144f8
Smirnov Alexander <alllecs@yandex.ru>
2014-12-10 14:27:44 +0400

    Компилируемая версия программы

--- isoread.c
+++ isoread.c
@@ -4,43 +4,32 @@
 #include <unistd.h>
 #include <sys/types.h>
 
-#include <cdio/cdio.h>
-#include <cdio/iso9660.h>
-
 int main(int argc, char *argv[])
 {
-	FILE *p_outfd;
-	CdioList_t *entlist;
-	iso9660_t *p_iso;
+	FILE *fp;
 
 	if (argc < 2) {
 		printf("Ошибка. Не указан файл.\n");
 		return 2;
 	}
 
-	p_iso = iso9660_open(argv[1]);
-
-	if (p_iso == NULL) {
-		perror("Ошибка при работе с файлом\n");
-		iso9660_close(p_iso);
+	fp = fopen(argv[1], "r");
+	if (fp == NULL) {
+		perror("fopen()");
 		return 3;
 	}
 
-	p_outfd = fopen(argv[1], "r");
-	if (p_outfd == NULL) {
-		perror("fopen()");
-		iso9660_close(p_iso);
-		return 4;
+	while (!feof(fp)) {
+		char n, k;
+
+		n = fscanf(fp, "%d", &k);
+		if (n == EOF) {
+			printf("EOF\n");
+		}
+		if (k != 32768)
+		printf("%c\n", k);
 	}
 
-	entlist = iso9660_ifs_readdir(p_iso, "/");
-
-	if (entlist) {
-		char filename[4096];
-		iso9660_stat_t *p_statbuf = (iso9660_stat_t *) _cdio_list_node_data;
-		iso9660_name_translate(p_statbuf->filename, filename);
-		printf ("-- /%s\n", filename);
-	}
 	return 0;
-	iso9660_close(p_iso);
+	fclose(fp);
 }

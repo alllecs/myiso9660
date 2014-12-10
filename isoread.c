777cbe49f735ab6c79fe06f1638db0af8c0d28ca
Smirnov Alexander <alllecs@yandex.ru>
2014-12-10 12:55:31 +0400

    Добавлен fopen

--- isoread.c
+++ isoread.c
@@ -10,6 +10,7 @@
 int main(int argc, char *argv[])
 {
 	FILE *p_outfd;
+	CdioList_t *entlist;
 	iso9660_t *p_iso;
 
 	if (argc < 2) {
@@ -20,15 +21,26 @@ int main(int argc, char *argv[])
 	p_iso = iso9660_open(argv[1]);
 
 	if (p_iso == NULL) {
-		perror("Ошибка при работе с фалом\n");
+		perror("Ошибка при работе с файлом\n");
 		iso9660_close(p_iso);
 		return 3;
 	}
 
-	if (!(p_outfd = fopen(argv[1], "wb"))) {
+	p_outfd = fopen(argv[1], "r");
+	if (p_outfd == NULL) {
 		perror("fopen()");
 		iso9660_close(p_iso);
 		return 4;
 	}
 
+	entlist = iso9660_ifs_readdir(p_iso, "/");
+
+	if (entlist) {
+		char filename[4096];
+		iso9660_stat_t *p_statbuf = (iso9660_stat_t *) _cdio_list_node_data;
+		iso9660_name_translate(p_statbuf->filename, filename);
+		printf ("-- /%s\n", filename);
+	}
+	return 0;
+	iso9660_close(p_iso);
 }

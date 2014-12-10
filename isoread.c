609d8182457b7a63a8f3d1d308c1096bdac545c1
Smirnov Alexander <alllecs@yandex.ru>
2014-12-10 12:25:19 +0400

    Добавлен оператор

--- isoread.c
+++ isoread.c
@@ -9,11 +9,26 @@
 
 int main(int argc, char *argv[])
 {
+	FILE *p_outfd;
 	iso9660_t *p_iso;
 
 	if (argc < 2) {
 		printf("Ошибка. Не указан файл.\n");
+		return 2;
+	}
+
+	p_iso = iso9660_open(argv[1]);
+
+	if (p_iso == NULL) {
+		perror("Ошибка при работе с фалом\n");
+		iso9660_close(p_iso);
 		return 3;
 	}
 
+	if (!(p_outfd = fopen(argv[1], "wb"))) {
+		perror("fopen()");
+		iso9660_close(p_iso);
+		return 4;
+	}
 
+}

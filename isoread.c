6f8a62876508034bf4d3bc9225db41c193e9d828
Smirnov Alexander <alllecs@yandex.ru>
2014-12-11 12:07:13 +0400

    Внесены изменения для чтения бинарного файла

--- isoread.c
+++ isoread.c
@@ -7,19 +7,39 @@
 int main(int argc, char *argv[])
 {
 	FILE *fp;
+	char *buf;
+	unsigned long fileLen;
 
 	if (argc < 2) {
 		printf("Ошибка. Не указан файл.\n");
 		return 2;
 	}
 
-	fp = fopen(argv[1], "r");
+	fp = fopen(argv[1], "rb");
 	if (fp == NULL) {
 		perror("fopen()");
 		return 3;
 	}
 
-	while (!feof(fp)) {
+	fseek(fp, 0, SEEK_END);
+	fileLen = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+
+	buf = (char *) malloc(fileLen + 1);
+	if (buf == NULL) {
+		fprintf(stderr, "Memory error!");
+			fclose(fp);
+		return 4;
+	}
+
+	fread(buf, fileLen, 1, fp);
+	fclose(fp);
+
+	free(buf);
+	return 0;
+}
+
+/*	while (!feof(fp)) {
 		char n, k;
 
 		n = fscanf(fp, "%d", &k);
@@ -33,3 +53,5 @@ int main(int argc, char *argv[])
 	return 0;
 	fclose(fp);
 }
+*/
+
